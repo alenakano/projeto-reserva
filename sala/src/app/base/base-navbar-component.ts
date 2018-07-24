4fc91236d4b42c2f6601d51561b16d718e59067c
Alexandre Nakano Marques <alexandre.nakano1@ibm.com>
2018-07-24 20:16:34 -0300

    base modules e navbar.

---
+++ sala/src/app/base/base-navbar-component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navbar',
+  templateUrl: './base-navbar.component.html'
+})
+export class BaseNavbarComponent {
+}
---
+++ sala/src/app/base/base.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { BaseNavbarComponent } from './base-navbar-component';
+
+@NgModule({
+  declarations: [
+    BaseNavbarComponent
+  ],
+  exports: [
+    BaseNavbarComponent
+  ],
+  providers: [],
+})
+export class BaseModule { }
